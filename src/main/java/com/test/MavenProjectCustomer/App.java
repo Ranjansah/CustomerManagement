577ebb1ded2335b00bdf877320d11574728dd783
Ranjansah <91522154+Ranjansah@users.noreply.github.com>
2022-08-05 12:17:54 +0530

    Update App.java

--- src/main/java/com/test/MavenProjectCustomer/App.java
+++ src/main/java/com/test/MavenProjectCustomer/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
          System.out.println( "Best of Luck!" );
         System.out.println( "This is my Second Milestone Assessment for DevOps Track!" );
     }
